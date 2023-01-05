33f030cc651500df24139395d8c3b485e1ebbfe7
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2023-01-05 11:42:53 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   const navigate = useNavigate();
   return (
     <div>
-      <h2>hello</h2>
-      <Link to="/invoices">Invoices</Link> |{" "}
-      <Link to="/expenses">Expenses</Link>
+      <div className="Invoices">
+        <Link to="/invoices">Invoices</Link>
+        <Link to="/expenses">Expenses</Link>
+      </div>
       <div>
         <button onClick={() => navigate(-1)}>Go back</button>
       </div>
